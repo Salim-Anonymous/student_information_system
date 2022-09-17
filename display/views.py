95f12446b7ec77bad1d17dc1184c55568f13951a
pravaat <pravaatchhetri66@gmail.com>
2022-09-18 03:54:30 +0600

    Done with registration form, card and search bar

--- display/views.py
+++ display/views.py
@@ -4,6 +4,6 @@ from django.http import HttpResponse
 from django.template import loader
 
 def index(request):
-    template = loader.get_template('users_display.html')
+    template = loader.get_template('studentForm.html')
     return HttpResponse(template.render())
 # Create your views here.
